bf8ca8e0274236e6d291990a2337093970c032fd
john4suresh <johnsuresh777@gmail.com>
2022-07-18 00:20:56 +0530

    Rendering Movies

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -1,8 +1,47 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import {
+  Box,
+  CircularProgress,
+  useMediaQuery,
+  Typography,
+} from "@mui/material";
+import { useSelector } from "react-redux";
+import {
+  useGetMoviesQuery,
+  useGetUpcomingMoviesQuery,
+} from "../../services/TMDB";
+import { MovieList } from "..";
 
 const Movies = () => {
-  console.log("Movies");
-  return <div>Movies</div>;
+  const { data: getMovies, error, isFetching } = useGetMoviesQuery();
+  const { data: upcomingData } = useGetUpcomingMoviesQuery();
+
+  if (isFetching) {
+    return (
+      <Box display={"flex"} justifyContent="center">
+        <CircularProgress size="4rem" />
+      </Box>
+    )
+  }
+
+  if (!getMovies.results.length) {
+    return (
+      <Box display={"flex"} justifyContent="center" alignItems={"center"} mt="20px">
+        <Typography variant="h4">
+          No movies that match that name.
+          <br />
+          Please search for something else.
+        </Typography>
+      </Box>
+    )
+  }
+
+  if (error) return 'An error has occured.'
+
+  return (
+    <div>
+      <MovieList movies={getMovies} />
+    </div>);
 };
 
 export default Movies;
--- src/index.js
+++ src/index.js
@@ -3,16 +3,20 @@ import ReactDOM from "react-dom/client";
 import App from "./components/App";
 import { BrowserRouter } from "react-router-dom";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { Provider } from "react-redux";
+import store from "./app/store";
 
 const theme = createTheme({});
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </ThemeProvider>
+    </Provider>
   </React.StrictMode>
 );
