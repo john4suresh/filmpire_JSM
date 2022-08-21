da78980f1ace9f0e819fc964b9cf8397ac36c470
john4suresh <johnsuresh777@gmail.com>
2022-08-21 16:18:40 +0530

    Work in Progress

--- src/features/currentGenreOrCategory.js
+++ src/features/currentGenreOrCategory.js
@@ -9,12 +9,15 @@ export const genreOrCategory = createSlice({
   },
   reducers: {
     selectGenreOrCategory: (state, action) => {
-      console.log(action.payload);
       state.genreIdOrCategoryName = action.payload;
+      state.searchQuery = '';
     },
+    searchMovie: (state, action) => {
+      state.searchQuery = action.payload;
+    }
   },
 });
 
-export const { selectGenreOrCategory } = genreOrCategory.actions;
+export const { selectGenreOrCategory, searchMovie } = genreOrCategory.actions;
 
 export default genreOrCategory.reducer;
